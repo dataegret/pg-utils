24746f2582b2dcccbb0c7121d239a41cecf7665c
Alexey Lesovsky <lesovsky@gmail.com>
2014-07-26 20:21:54 +0600

    add cache_stat.sql

---
+++ sql/cache_stat.sql
@@ -0,0 +1,15 @@
+-- Show shared_buffers and os pagecache stat for current database
+-- Require pg_buffercache and pgfincore
+WITH qq AS (SELECT
+  c.oid,
+  count(b.bufferid) * 8192 AS size,
+  (pgfincore(c.oid::regclass)).pages_mem * 4096 as size_in_pagecache
+FROM pg_buffercache b
+INNER JOIN pg_class c ON b.relfilenode = pg_relation_filenode(c.oid)
+AND b.reldatabase IN (0, (SELECT oid FROM pg_database WHERE datname = current_database()))
+GROUP BY 1)
+SELECT
+  pg_size_pretty(sum(qq.size)) AS shared_buffers_size,
+  pg_size_pretty(sum(qq.size_in_pagecache)) AS size_in_pagecache,
+  pg_size_pretty(pg_database_size(current_database())) as database_size
+FROM qq;
