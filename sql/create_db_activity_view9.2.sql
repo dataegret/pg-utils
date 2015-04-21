341b02cee9151d527d7ade641b2a51993c951471
Alexey Ermakov <alexius.work@gmail.com>
2015-04-21 17:44:49 +0600

    show aborted transactions in db_activity view

--- sql/create_db_activity_view9.2.sql
+++ sql/create_db_activity_view9.2.sql
@@ -1,11 +1,7 @@
-
-
 CREATE OR REPLACE VIEW db_activity AS
-    SELECT (now() - pg_stat_activity.xact_start) AS ts_age, pg_stat_activity.state, (now() - pg_stat_activity.query_start) as query_age, (now() - state_change) as change_age, pg_stat_activity.datname, pg_stat_activity.pid, pg_stat_activity.usename, pg_stat_activity.waiting, pg_stat_activity.client_addr, pg_stat_activity.client_port, pg_stat_activity.query 
-FROM pg_stat_activity 
-WHERE 
-((now() - pg_stat_activity.xact_start) > '00:00:00.1'::interval)
--- OR ((now() - pg_stat_activity.query_start)> '00:00:00.5'::interval)
+SELECT (clock_timestamp() - pg_stat_activity.xact_start) AS ts_age, pg_stat_activity.state, (clock_timestamp() - pg_stat_activity.query_start) as query_age, (clock_timestamp() - state_change) as change_age, pg_stat_activity.datname, pg_stat_activity.pid, pg_stat_activity.usename, pg_stat_activity.waiting, pg_stat_activity.client_addr, pg_stat_activity.client_port, pg_stat_activity.query
+FROM pg_stat_activity
+WHERE
+((clock_timestamp() - pg_stat_activity.xact_start > '00:00:00.1'::interval) OR (clock_timestamp() - pg_stat_activity.query_start > '00:00:00.1'::interval and state = 'idle in transaction (aborted)'))
 and pg_stat_activity.pid<>pg_backend_pid()
-ORDER BY pg_stat_activity.xact_start;
-
+ORDER BY coalesce(pg_stat_activity.xact_start, pg_stat_activity.query_start);
