a1f59d36061ed47368444b607952a9a562a50d20
Alexey Ermakov <alexius.work@gmail.com>
2016-05-23 18:07:21 +0600

    add matviews support to set_missing_grants.sql

--- sql/set_missing_grants.sql
+++ sql/set_missing_grants.sql
@@ -5,13 +5,15 @@ SELECT
                         'GRANT SELECT ON TABLE "'||n.nspname||'"."'||c.relname||'" TO role_ro;'
                 WHEN 'v' THEN
                         'GRANT SELECT ON TABLE "'||n.nspname||'"."'||c.relname||'" TO role_ro;'
+                WHEN 'm' THEN
+                        'GRANT SELECT ON TABLE "'||n.nspname||'"."'||c.relname||'" TO role_ro;'
                 WHEN 'S' THEN
                         'GRANT SELECT ON SEQUENCE "'||n.nspname||'"."'||c.relname||'" TO role_ro;'
         END as "NEW GRANT"
 FROM pg_catalog.pg_class c
 LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
 WHERE
-        c.relkind IN ('r', 'v', 'S')
+        c.relkind IN ('r', 'v', 'm', 'S')
         AND n.nspname !~ '^pg_'
         AND n.nspname not in ('information_schema', '_slony')
 AND (
@@ -20,6 +22,7 @@ AND (
         OR CASE c.relkind
         WHEN 'r' THEN c.relacl::text not like '%role_rw=arwd/%'
         WHEN 'v' THEN c.relacl::text not like '%role_rw=r/%'
+        WHEN 'm' THEN c.relacl::text not like '%role_rw=r/%'
         WHEN 'S' THEN c.relacl::text not like '%role_rw=rU/%'
         END
 )
@@ -30,13 +33,15 @@ SELECT
                         'GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE "'||n.nspname||'"."'||c.relname||'" TO role_rw;'
                 WHEN 'v' THEN
                         'GRANT SELECT ON TABLE "'||n.nspname||'"."'||c.relname||'" TO role_rw;'
+                WHEN 'm' THEN
+                        'GRANT SELECT ON TABLE "'||n.nspname||'"."'||c.relname||'" TO role_rw;'
                 WHEN 'S' THEN
                         'GRANT SELECT,USAGE ON SEQUENCE "'||n.nspname||'"."'||c.relname||'" TO role_rw;'
         END as "NEW GRANT"
 FROM pg_catalog.pg_class c
 LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
 WHERE
-        c.relkind IN ('r', 'v', 'S')
+        c.relkind IN ('r', 'v', 'm', 'S')
         AND n.nspname !~ '^pg_'
         AND n.nspname not in ('information_schema', '_slony')
 AND (
@@ -45,6 +50,7 @@ AND (
         OR CASE c.relkind
         WHEN 'r' THEN c.relacl::text not like '%role_rw=arwd/%'
         WHEN 'v' THEN c.relacl::text not like '%role_rw=r/%'
+        WHEN 'm' THEN c.relacl::text not like '%role_rw=r/%'
         WHEN 'S' THEN c.relacl::text not like '%role_rw=rU/%'
         END
 )
