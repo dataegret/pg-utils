ade1c9af5a04a2be12cc0cfe08b3069bb57fa29c
Alexey Lesovsky <lesovsky@gmail.com>
2017-02-12 11:06:10 +0500

    repl_activity: add xact/time ages of standby's oldest query

--- sql/create_repl_activity_view9.5.sql
+++ sql/create_repl_activity_view9.5.sql
@@ -8,4 +8,6 @@ CREATE VIEW repl_activity AS SELECT
     (pg_xlog_location_diff(write_location,flush_location) / 1024)::bigint as flush,
     (pg_xlog_location_diff(flush_location,replay_location) / 1024)::bigint as replay,
     (pg_xlog_location_diff(pg_current_xlog_location(),replay_location))::bigint / 1024 as total_lag
+    (pg_last_committed_xact()).xid::text::bigint - backend_xmin::text::bigint as xact_age,
+    (pg_last_committed_xact()).timestamp - pg_xact_commit_timestamp(backend_xmin) as time_age
 FROM pg_stat_replication;
