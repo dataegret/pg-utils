a38f24f882b3c2e1fed316a2a410b8c5548a53c6
Victor Yegorov <vyegorov@gmail.com>
2018-10-31 20:43:04 +0200

    Add deadlock detection
    It might happen that all lockers are also waiting on some other resources,
    which leads to empty output.
    Properly detect locking leads via another recursive check on lockers only.

--- sql/locktree.sql
+++ sql/locktree.sql
@@ -7,11 +7,17 @@ WITH RECURSIVE l AS (
     FROM l w JOIN l ON l.obj IS NOT DISTINCT FROM w.obj AND l.locktype=w.locktype AND NOT l.pid=w.pid AND l.granted
    WHERE NOT w.granted
      AND NOT EXISTS ( SELECT FROM l i WHERE i.pid=l.pid AND i.locktype=l.locktype AND i.obj IS NOT DISTINCT FROM l.obj AND i.m > l.m )
-), tree AS (
-  SELECT l.locker pid, l.locker root, NULL::record obj, NULL::int AS m, 0 lvl, locker::text path, array_agg(l.locker) OVER () all_pids
-    FROM ( SELECT DISTINCT locker FROM pairs l WHERE NOT EXISTS (SELECT 1 FROM pairs WHERE waiter=l.locker) ) l
+), leads AS (
+  SELECT o.locker, o.m, 1::int lvl, count(*) q, ARRAY[locker] track, false AS cycle FROM pairs o GROUP BY o.locker, o.m
   UNION ALL
-  SELECT w.waiter pid, tree.root, w.obj, w.m, tree.lvl+1, tree.path||'.'||w.waiter, all_pids || array_agg(w.waiter) OVER ()
+  SELECT i.locker, i.m, leads.lvl+1, (SELECT count(*) FROM pairs q WHERE q.locker=i.locker), leads.track||i.locker, i.locker=ANY(leads.track||i.locker)
+    FROM pairs i, leads WHERE i.waiter=leads.locker AND NOT cycle
+), tree AS (
+  SELECT locker pid, locker root, CASE WHEN cycle THEN track END dl, NULL::record obj, m, 0 lvl, locker::text path, array_agg(locker) OVER () all_pids FROM leads o
+   WHERE (cycle AND NOT EXISTS (SELECT FROM leads i WHERE i.locker=ANY(o.track) AND (i.lvl>o.lvl OR i.q<o.q)))
+      OR (NOT cycle AND NOT EXISTS (SELECT FROM pairs WHERE waiter=o.locker) AND NOT EXISTS (SELECT FROM leads i WHERE i.locker=o.locker AND i.m>o.m))
+  UNION ALL
+  SELECT w.waiter pid, tree.root, CASE WHEN w.waiter=ANY(tree.dl) THEN tree.dl END, w.obj, w.m, tree.lvl+1, tree.path||'.'||w.waiter, all_pids || array_agg(w.waiter) OVER ()
     FROM tree JOIN pairs w ON tree.pid=w.locker AND NOT w.waiter = ANY ( all_pids )
 )
 SELECT (clock_timestamp() - a.xact_start)::interval(3) AS ts_age,
