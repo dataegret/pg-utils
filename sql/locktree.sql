4dbb55ec9d85a3ccdc9e5903adf24ace68a6f788
Victor Yegorov <vyegorov@gmail.com>
2018-11-16 01:16:47 +0200

    Provide `wait_on_object` column in the output
    It might desirable to see which object we're waiting on.
    To do so join to the pairs by both, locker and waiter, otherwise
    duplicates are prodcued.
    To do so add `dad` column to the `tree` subquery.
    
    It was tempting to also change database and relation (wherever possible)
    oids into names, but I choose not to do so.

--- sql/locktree.sql
+++ sql/locktree.sql
@@ -13,20 +13,22 @@ WITH RECURSIVE l AS (
   SELECT i.locker, leads.lvl+1, (SELECT count(*) FROM pairs q WHERE q.locker=i.locker), leads.track||i.locker, i.locker=ANY(leads.track)
     FROM pairs i, leads WHERE i.waiter=leads.locker AND NOT cycle
 ), tree AS (
-  SELECT locker pid, locker root, CASE WHEN cycle THEN track END dl, NULL::record obj, 0 lvl, locker::text path, array_agg(locker) OVER () all_pids FROM leads o
+  SELECT locker pid,locker dad,locker root,CASE WHEN cycle THEN track END dl, NULL::record obj,0 lvl,locker::text path,array_agg(locker) OVER () all_pids FROM leads o
    WHERE (cycle AND NOT EXISTS (SELECT FROM leads i WHERE i.locker=ANY(o.track) AND (i.lvl>o.lvl OR i.q<o.q)))
       OR (NOT cycle AND NOT EXISTS (SELECT FROM pairs WHERE waiter=o.locker) AND NOT EXISTS (SELECT FROM leads i WHERE i.locker=o.locker AND i.lvl>o.lvl))
   UNION ALL
-  SELECT w.waiter pid, tree.root, CASE WHEN w.waiter=ANY(tree.dl) THEN tree.dl END, w.obj, tree.lvl+1, tree.path||'.'||w.waiter, all_pids || array_agg(w.waiter) OVER ()
+  SELECT w.waiter pid,tree.pid,tree.root,CASE WHEN w.waiter=ANY(tree.dl) THEN tree.dl END,w.obj,tree.lvl+1,tree.path||'.'||w.waiter,all_pids || array_agg(w.waiter) OVER ()
     FROM tree JOIN pairs w ON tree.pid=w.locker AND NOT w.waiter = ANY ( all_pids )
 )
 SELECT (clock_timestamp() - a.xact_start)::interval(0) AS ts_age,
        (clock_timestamp() - a.state_change)::interval(0) AS change_age,
-       a.datname,a.usename,a.client_addr,tree.pid,
-       replace(a.state, 'idle in transaction', 'idletx') state,
+       a.datname,a.usename,a.client_addr,
+       --w.obj wait_on_object,
+       tree.pid,replace(a.state, 'idle in transaction', 'idletx') state,
        lvl,(SELECT count(*) FROM tree p WHERE p.path ~ ('^'||tree.path) AND NOT p.path=tree.path) blocked,
        CASE WHEN tree.pid=ANY(tree.dl) THEN '!>' ELSE repeat(' .', lvl) END||' '||trim(left(regexp_replace(a.query, E'\\s+', ' ', 'g'),100)) query
   FROM tree
+  LEFT JOIN pairs w ON w.waiter=tree.pid AND w.locker=tree.dad
   JOIN pg_stat_activity a USING (pid)
   JOIN pg_stat_activity r ON r.pid=tree.root
  ORDER BY (now() - r.xact_start), path;
