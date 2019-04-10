56ad316cee7c0da23beefb58458a02b46a1a7a86
Alexey Ermakov <alexius.work@gmail.com>
2019-04-10 12:26:56 +0600

    makecron.sh: fix issues

--- bin/makecron.sh
+++ bin/makecron.sh
@@ -15,7 +15,7 @@ IFS=' ' read -r -a EMAILS <<< $EMAILSLIST
 MAIN_EMAIL=${EMAILS[0]}
 
 # List of connected standby nodes.
-STANDBYLIST=$(psql -qAtX -U postgres -c "select string_agg(host(client_addr), ' ') from pg_stat_replication")
+STANDBYLIST=$(psql -qAtX -c "select string_agg(host(client_addr), ' ') from pg_stat_replication")
 
 # Customer name.
 CUSTOMER_NAME=$(echo $MAIN_EMAIL |cut -d" " -f1 |cut -d@ -f1 |cut -d+ -f2)
@@ -24,11 +24,11 @@ CUSTOMER_NAME=$(echo $MAIN_EMAIL |cut -d" " -f1 |cut -d@ -f1 |cut -d+ -f2)
 CUSTOMER_NAME_TITLE=$(echo $CUSTOMER_NAME |sed 's/[^ ]\+/\L\u&/g')
 
 # An absolute path to Postgres log files, suppose logging_collector already enabled.
-LOG_DIRECTORY=$(psql -qAtX -U postgres -c "select case when left(ld,1) = '/' then ld else dd||'/'||ld end as log_directory from current_setting('data_directory') dd, current_setting('log_directory') ld")
+LOG_DIRECTORY=$(psql -qAtX -c "select (case when left(ld,1) = '/' then ld else dd||'/'||ld end) || '/' as log_directory from current_setting('data_directory') dd, current_setting('log_directory') ld")
 
 # Hours in current timezone shifted to Europe/Moscow timezone.
-RESET_HOUR=$(date --date='TZ="Europe/Moscow" 23' +"%H")
-REPORT_HOUR=$(date --date='TZ="Europe/Moscow" 00' +"%H")
+REPORT_HOUR=$(date --date='TZ="Europe/Moscow" 23' +"%H")
+RESET_HOUR=$(date --date='TZ="Europe/Moscow" 00' +"%H")
 DELETE_HOUR=$(date --date='TZ="Europe/Moscow" 04' +"%H")
 
 # Sanity check
@@ -37,24 +37,23 @@ DELETE_HOUR=$(date --date='TZ="Europe/Moscow" 04' +"%H")
 
 envsubst <<EOF
 MAILTO=$MAIN_EMAIL
-# all times given in Europe/Moscow timezone
 
 # pg_stat_statements report
-59 $REPORT_HOUR * * *	/usr/bin/psql -XAt -U postgres -f ~/stuff/sql/global_reports/query_stat_total.sql | /usr/bin/mail -e -s "Daily report of pg_stat_statements for \`/bin/date "+\%Y-\%m-\%d"\` from \`hostname\` database at $CUSTOMER_NAME_TITLE" $EMAILSLIST'
+58 $REPORT_HOUR * * *	/usr/bin/psql -XAt -f ~/stuff/sql/global_reports/query_stat_total.sql | /usr/bin/mail -e -s "Daily report of pg_stat_statements for \`/bin/date -u "+\%Y-\%m-\%d"\` from \`hostname\` database at $CUSTOMER_NAME_TITLE" $EMAILSLIST
 
 # pg_stat_statements reset
 00 $RESET_HOUR * * *	/usr/bin/psql -t -c "select pg_stat_statements_reset()" > /dev/null
 
 # Terminate long transactions
-#* * * * *	/usr/bin/psql -Xxt1 -c "SELECT pg_terminate_backend(pid),now(),now()-xact_start as duration,* from pg_stat_activity where (now() - pg_stat_activity.xact_start) > '60 min'::interval and state<>'idle' and client_addr is not null and usename not in ('postgres', 'backuper', 'replica')" | grep -vE '^(|\(No rows\))$'
+#* * * * *	/usr/bin/psql -Xxt1 -c "SELECT pg_terminate_backend(pid),now(),now()-xact_start as duration,* from pg_stat_activity where (now() - pg_stat_activity.xact_start) > '60 min'::interval and state<>'idle' and usename not in ('postgres', 'backuper', 'replica')" | grep -vE '^(|\(No rows\))$'
 
 # Delete old log files
 00 $DELETE_HOUR  * * *	find $LOG_DIRECTORY -name 'post*.log*' -type f -mtime +7 -delete
 
 # Lower io priority
-* * * * *       ps -u postgres x | grep -Ei 'autovacuum|COPY|pg_dump|clickcast_background|: logger process|: checkpointer process|: writer process|: autovacuum launcher process|: stats collector process|lzop|wal-e' | grep -vE 'grep|pgbouncer' | perl -pe 's/^\s*(\d+) .*$/\$1/' | xargs --no-run-if-empty -I $ ionice -c 3 -t -p $
+* * * * *       ps -u postgres x | grep -Ei 'autovacuum|COPY|pg_dump|: logger process|: checkpointer process|: writer process|: autovacuum launcher process|: stats collector process|lzop|wal-e' | grep -vE 'grep|pgbouncer' | perl -pe 's/^\s*(\d+) .*$/\$1/' | xargs --no-run-if-empty -I $ ionice -c 3 -t -p $
 # Lower cpu priority
-* * * * *       ps -u postgres x | grep -Ei 'autovacuum|COPY|pg_dump|clickcast_background|: logger process|: checkpointer process|: writer process|: autovacuum launcher process|: stats collector process|lzop|wal-e' | grep -vE 'grep|pgbouncer' | perl -pe 's/^\s*(\d+) .*$/\$1/' | xargs --no-run-if-empty -I $ renice -n 20 -p $ >/dev/null 2>/dev/null
+* * * * *       ps -u postgres x | grep -Ei 'autovacuum|COPY|pg_dump|: logger process|: checkpointer process|: writer process|: autovacuum launcher process|: stats collector process|lzop|wal-e' | grep -vE 'grep|pgbouncer' | perl -pe 's/^\s*(\d+) .*$/\$1/' | xargs --no-run-if-empty -I $ renice -n 20 -p $ >/dev/null 2>/dev/null
 EOF
 
 if [[ -n $STANDBYLIST ]]; then
