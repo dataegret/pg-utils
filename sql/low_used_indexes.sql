e3a3835900d9d60cc66d2c38d9e7dccead61334f
Andrey-Salnikov <andrey.salnikov@dataegret.com>
2019-07-03 17:32:34 +0300

    Modified script for proper checking low used indecies on replica servers.

--- sql/low_used_indexes.sql
+++ sql/low_used_indexes.sql
@@ -1,21 +1,28 @@
 
-SELECT
-        pg_stat_user_indexes.schemaname||'.'||pg_stat_user_indexes.relname,
-        indexrelname,
-        pg_stat_user_indexes.idx_scan,
-        (coalesce(n_tup_ins,0)+coalesce(n_tup_upd,0)-coalesce(n_tup_hot_upd,0)+coalesce(n_tup_del,0)) as write_activity,
-        pg_stat_user_tables.seq_scan,
-        pg_stat_user_tables.n_live_tup,
-	pg_size_pretty(pg_relation_size(pg_index.indexrelid::regclass)) as size
-from pg_stat_user_indexes
-join pg_stat_user_tables
-        on pg_stat_user_indexes.relid=pg_stat_user_tables.relid
-join pg_index
-        ON pg_index.indexrelid=pg_stat_user_indexes.indexrelid
-where
-        pg_index.indisunique is false
-        and pg_stat_user_indexes.idx_scan::float/(coalesce(n_tup_ins,0)+coalesce(n_tup_upd,0)-coalesce(n_tup_hot_upd,0)+coalesce(n_tup_del,0)+1)::float<0.01
-        and (coalesce(n_tup_ins,0)+coalesce(n_tup_upd,0)-coalesce(n_tup_hot_upd,0)+coalesce(n_tup_del,0))>10000
-order by 4 desc,1,2
+SELECT pg_stat_user_indexes.schemaname || '.' || pg_stat_user_indexes.relname tablemane
+     , pg_stat_user_indexes.indexrelname
+     , pg_stat_user_indexes.idx_scan
+     , psut.write_activity
+     , psut.seq_scan
+     , psut.n_live_tup
+     , pg_size_pretty (pg_relation_size (pg_index.indexrelid::regclass)) as size
 
+  from pg_stat_user_indexes
+  join pg_index
+    ON pg_stat_user_indexes.indexrelid = pg_index.indexrelid
 
+  join (select pg_stat_user_tables.relid
+             , pg_stat_user_tables.seq_scan
+             , pg_stat_user_tables.n_live_tup
+             , ( coalesce (pg_stat_user_tables.n_tup_ins, 0)
+               + coalesce (pg_stat_user_tables.n_tup_upd, 0)
+               - coalesce (pg_stat_user_tables.n_tup_hot_upd, 0)
+               + coalesce (pg_stat_user_tables.n_tup_del, 0)
+               ) as write_activity
+          from pg_stat_user_tables) psut
+    on pg_stat_user_indexes.relid = psut.relid
+
+ where pg_index.indisunique is false
+   and pg_stat_user_indexes.idx_scan::float / (psut.write_activity + 1)::float < 0.01
+   and psut.write_activity > case when pg_is_in_recovery () then -1 else 10000 end
+  order by 4 desc, 1, 2
