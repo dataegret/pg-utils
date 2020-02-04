d9e225fd6fb855f92c1c7a695a9959fb09d0d476
Alexey Ermakov <alexius.work@gmail.com>
2020-02-04 17:19:42 +0600

    show queries from dropped users and databases as from "unknown" instead of empty rows.

--- sql/global_reports/query_stat_total.sql
+++ sql/global_reports/query_stat_total.sql
@@ -12,18 +12,18 @@ with pg_stat_statements_normalized as (
     E'\r', '')
     as query_normalized
     --if current database is postgres then generate report for all databases otherwise generate for current database only
-    from pg_stat_statements where dbid in (SELECT oid from pg_database where current_database() = 'postgres' or datname=current_database())
+    from pg_stat_statements where current_database() = 'postgres' or dbid in (SELECT oid from pg_database where datname=current_database())
 ),
 totals as (
     select sum(total_time) AS total_time, sum(blk_read_time+blk_write_time) as io_time,
     sum(total_time-blk_read_time-blk_write_time) as cpu_time, sum(calls) AS ncalls,
     sum(rows) as total_rows FROM pg_stat_statements
-    WHERE dbid in (SELECT oid from pg_database where current_database() = 'postgres' or datname=current_database())
+    WHERE current_database() = 'postgres' or dbid in (SELECT oid from pg_database where datname=current_database())
 ),
 _pg_stat_statements as (
     select
-    (select datname from pg_database where oid = p.dbid) as database,
-    (select rolname from pg_roles where oid = p.userid) as username,
+    coalesce((select datname from pg_database where oid = p.dbid), 'unknown') as database,
+    coalesce((select rolname from pg_roles where oid = p.userid), 'unknown') as username,
     --select shortest query, replace \n\n-- strings to avoid email clients format text as footer
     substring(
     translate(
