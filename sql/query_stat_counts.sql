008af53542202d9e4f9057a3f54fe65f4dfa3099
Nikitin-Alexandr <79432079+Nikitin-Alexandr@users.noreply.github.com>
2021-07-07 18:06:47 +0700

    Update query_stat_counts.sql

--- sql/query_stat_counts.sql
+++ sql/query_stat_counts.sql
@@ -2,13 +2,13 @@
 with s AS
 (SELECT sum(total_time) AS t,sum(calls) AS s,sum(rows) as r FROM pg_stat_statements WHERE dbid=(SELECT oid from pg_database where datname=current_database()))
 SELECT
-(100*total_time/(SELECT t FROM s))::numeric(20,2) AS time_percent,
+(100*total_time/(SELECT t FROM s))::numeric(20,2) AS "time_%",
 total_time::numeric(20,2) as total_time,
 (total_time*1000/calls)::numeric(10,3) AS avg_time,
 calls,
-(100*calls/(SELECT s FROM s))::numeric(20,2) AS calls_percent,
+(100*calls/(SELECT s FROM s))::numeric(20,2) AS "call_%",
 rows,
-(100*rows/(SELECT r from s))::numeric(20,2) AS row_percent,
+(100*rows/(SELECT r from s))::numeric(20,2) AS "row_%",
 query
 FROM pg_stat_statements
 WHERE
@@ -18,14 +18,14 @@ AND dbid=(SELECT oid from pg_database where datname=current_database())
 UNION all
 
 SELECT
-(100*sum(total_time)/(SELECT t FROM s))::numeric(20,2) AS time_percent,
+(100*sum(total_time)/(SELECT t FROM s))::numeric(20,2),
 sum(total_time)::numeric(20,2),
-(sum(total_time)*1000/sum(calls))::numeric(10,3) AS avg_time,
+(sum(total_time)*1000/sum(calls))::numeric(10,3),
 sum(calls),
-(100*sum(calls)/(SELECT s FROM s))::numeric(20,2) AS calls_percent,
+(100*sum(calls)/(SELECT s FROM s))::numeric(20,2),
 sum(rows),
-(100*sum(rows)/(SELECT r from s))::numeric(20,2) AS row_percent,
-'other' AS query
+(100*sum(rows)/(SELECT r from s))::numeric(20,2),
+'other'
 FROM pg_stat_statements
 WHERE
 calls/(SELECT s FROM s)<0.01
